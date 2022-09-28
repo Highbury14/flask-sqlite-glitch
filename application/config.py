3c6fc9c2a6fadca95b8207860ec6e60b81090bbb
Georgie Jacob <56916479+Highbury14@users.noreply.github.com>
2022-09-28 13:16:52 +0530

    SQlite db-file path corrected in config-settings.

--- application/config.py
+++ application/config.py
@@ -8,7 +8,7 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class LocalDevelopmentConfig(Config):
-    SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
+    SQLITE_DB_DIR = os.path.join(basedir, "../db_directory/")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR, "testdb.sqlite3")
     DEBUG = True
     SECRET_KEY = os.environ['SECRET_KEY'] if ('SECRET_KEY' in os.environ) else '123'
